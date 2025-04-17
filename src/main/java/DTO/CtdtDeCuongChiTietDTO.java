a120ca9c19a1c40889f5293e2079b1dc16e35489
daokhang72 <151323420+daokhang72@users.noreply.github.com>
2025-04-17 21:15:51 +0700

    update DTO

--- src/main/java/DTO/CtdtDeCuongChiTietDTO.java
+++ src/main/java/DTO/CtdtDeCuongChiTietDTO.java
@@ -1,7 +1,16 @@
 package DTO;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class CtdtDeCuongChiTietDTO {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int deCuongId;
+	
     private int maHp;
     private String tenBoPhan;
     private String diemDanhGia;
