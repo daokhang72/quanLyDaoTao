320df25b3f6daaa7dceae9575becf2dd6985a81c
camdao <luongcamdao@gmail.com>
2025-04-18 21:03:23 +0700

    feat: KeHoachDayHoc-CRUD

---
+++ src/main/java/com/mhpl/QLDaoTao/repository/KeHoachDayHocRepository.java
@@ -0,0 +1,7 @@
+package com.mhpl.QLDaoTao.repository;
+
+import com.mhpl.QLDaoTao.model.KeHoachDayHoc;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KeHoachDayHocRepository extends JpaRepository<KeHoachDayHoc, Integer> {
+}
