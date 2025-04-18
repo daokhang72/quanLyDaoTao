9b67a5bbe537727820e30de28086f70a84b66d0c
camdao <luongcamdao@gmail.com>
2025-04-18 21:56:39 +0700

    feat: PhanCongGiangDay-CRUD

---
+++ src/main/java/com/mhpl/QLDaoTao/repository/PhanCongGiangDayRepository.java
@@ -0,0 +1,7 @@
+package com.mhpl.QLDaoTao.repository;
+
+import com.mhpl.QLDaoTao.model.PhanCongGiangDay;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhanCongGiangDayRepository extends JpaRepository<PhanCongGiangDay, Integer> {
+}
