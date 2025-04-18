c82fbfca73ec90903493678692bf70d7af3e22a8
camdao <luongcamdao@gmail.com>
2025-04-18 20:55:03 +0700

    feat: DeCUongChiTiet-CRUD

---
+++ src/main/java/com/mhpl/QLDaoTao/dto/request/DeCuongChiTietRequest.java
@@ -0,0 +1,9 @@
+package com.mhpl.QLDaoTao.dto.request;
+
+public record DeCuongChiTietRequest(
+        int maHp,
+        String tenBoPhan,
+        String diemDanhGia,
+        Float trongSo,
+        String hinhThucDanhGia
+) {}
---
+++ src/main/java/com/mhpl/QLDaoTao/repository/DeCuongChiTietRepository.java
@@ -0,0 +1,7 @@
+package com.mhpl.QLDaoTao.repository;
+
+import com.mhpl.QLDaoTao.model.DeCuongChiTiet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeCuongChiTietRepository extends JpaRepository<DeCuongChiTiet, Integer> {
+}
