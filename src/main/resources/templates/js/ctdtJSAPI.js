85f7e77ad1110ceb4f67e5ebbadc6d68998912c0
camdao <luongcamdao@gmail.com>
2025-05-03 16:45:03 +0700

    feat: add CTDT html

--- src/main/resources/templates/js/ctdtJSAPI.js
+++ src/main/resources/templates/js/ctdtJSAPI.js
@@ -1,7 +1,22 @@
 const URL = 'http://localhost:8081'
 function getAllCTDT() {
-    return fetch(URL+'/thongtin')
+    return fetch(`${URL}/thongtin`)
         .then(response => response.json())
+        .catch(error => {
+            console.error('Lỗi:', error);
+            throw error;
+        });
+}
+function deleteCTDTByid(id) {
+    return fetch(`${URL}/thongtin/${id}`, {
+        method: 'DELETE',
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Xóa thất bại');
+            }
+            return response.json();
+        })
         .catch(error => {
             console.error('Lỗi:', error);
             throw error;
