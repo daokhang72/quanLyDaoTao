22315b96c9e0f10d14f10331f0594bd9d88c9979
daokhang72 <151323420+daokhang72@users.noreply.github.com>
2025-05-05 09:47:44 +0700

    update active

---
+++ src/main/java/com/QLDaoTao/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.QLDaoTao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping("/home")
+    public String homeAdmin() {
+        return "homeAdmin";
+    }
+
+    @GetMapping("/ctdt")
+    public String ctdtAdmin() {
+        return "CTDTAdmin";
+    }
+}
