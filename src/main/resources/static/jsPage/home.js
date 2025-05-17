121d3ad406bb97d6d58932dbe023b61d9ead2a2a
camdao <luongcamdao@gmail.com>
2025-05-17 19:36:44 +0700

    chore: refactor structure

---
+++ src/main/resources/static/jsPage/home.js
@@ -0,0 +1,4 @@
+export default function createHome() {
+    return `<h1 class="welcome">CHÀO MỪNG BẠN ĐẾN VỚI<br> HỆ THỐNG QUẢN LÝ <br>CHƯƠNG TRÌNH ĐÀO TẠO</h1>
+      <p class="sub-welcome">ĐÂY LÀ NƠI BẠN CÓ THỂ CẬP NHẬT VÀ THEO DÕI <br>TOÀN BỘ CHƯƠNG TRÌNH ĐÀO TẠO CỦA TRƯỜNG</p>`;
+}
